e772bf19445ff60b89f0fce00fe8756de3eb9f1e
Viktor Poliushko <milleniiumkb@gmail.com>
2020-06-15 15:29:32 +0300

    fresh react project (no  security vulnerability)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 function App() {
   return (
-    <div className="container">
-
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          FORM
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          POSTS
+        </div>
+        <div className="col">
+          FETCHED POSTS
+        </div>
+      </div>
     </div>
   );
 }
