db245b62edee1604af4dbfc0e426fe3e9a0b3272
victorpoliushko <milleniiumkb@gmail.com>
2020-11-24 17:07:23 +0200

    form created

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
 export default class PostForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: "",
+    };
+  }
+
   submitHandler = (event) => {
     event.preventDefault();
+
+    const { title } = this.state;
+
+    const newPost = {
+      title,
+      id: Date.now().toString(),
+    };
+
+    this.setState({ title: "" });
+  };
+
+  changeInputHandler = (event) => {
+    event.persist();
+    this.setState((prev) => ({
+      ...prev,
+      ...{
+        [event.target.name]: event.target.value,
+      },
+    }));
   };
 
   render() {
@@ -10,9 +37,18 @@ export default class PostForm extends Component {
       <form onSubmit={this.submitHandler}>
         <div className="form-group">
           <label htmlFor="title">Title</label>
-          <input type="text" class="form-control" id="title" />
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            name="title"
+            value={this.state.title}
+            onChange={this.changeInputHandler}
+          />
         </div>
-        <button className="btn btn-success" type="submit"></button>
+        <button className="btn btn-success" type="submit">
+          Create
+        </button>
       </form>
     );
   }
